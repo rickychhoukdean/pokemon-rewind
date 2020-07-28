581eeaadd04b635f6b1ffe33149acf898d4cea88
rickychhoukdean <chhoukdean184@msn.com>
2020-07-28 09:52:16 -0400

    Setup inital structure of file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  //Pokemonset made variable incase we wanted to look at different sets in the future
+  const pokemonSet = 4;
+
+  async function createBackup() {
+    const cardData = await fetch(
+      `https://api.pokemontcg.io/v1/cards?setCode=base${pokemonSet}`
+    ).then((res) => res.json());
+    console.log(cardData);
+    setResponse("Backup created");
+  }
+
+  function purgeBackup() {
+    setResponse("Backup deleted");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Pokemon Set {pokemonSet}</h2>
+
+      <button
+        onClick={() => {
+          createBackup();
+        }}
+      >
+        Create backup
+      </button>
+      <button
+        onClick={() => {
+          purgeBackup();
+        }}
+      >
+        Purge Backup
+      </button>
+      <button>Search backup</button>
+      <div>{response}</div>
     </div>
   );
 }
